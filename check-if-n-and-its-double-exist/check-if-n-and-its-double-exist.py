3c6dce263637e884cfc256e9d3df7be17c63d413
jxxyyyn <142684491+jxxyyyn@users.noreply.github.com>
2024-06-19 22:53:14 +0900

    Time: 79 ms, Memory: 11.7 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def checkIfExist(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        for i in range(len(arr)) :
+            for j in range(len(arr)):
+                if i != j :
+                    if arr[i] == arr[j] * 2:
+                        return True
+        return False
