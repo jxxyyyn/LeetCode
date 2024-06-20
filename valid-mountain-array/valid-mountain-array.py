f9cdac97fc61af09e614f80bbd476d04589adc81
jxxyyyn <142684491+jxxyyyn@users.noreply.github.com>
2024-06-20 19:55:07 +0900

    Time: 145 ms, Memory: 12.9 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -5,15 +5,15 @@ class Solution(object):
         :rtype: bool
         """
         
+        i = 0
+        j = len(arr) - 1
+        
         if len(arr) < 3 :
             return False
-        
-        l = 0
-        r = len(arr) - 1
-        
-        while l+1 < r and arr[l] < arr[l+1] :
-            l += 1
-        while r-1 > 0 and arr[r] < arr[r-1] :
-            r -= 1
             
-        return l == r
+        while i+1 < j and arr[i] < arr[i+1] : 
+            i += 1
+        while 0 < j-1 and arr[j] < arr[j-1] : 
+            j -= 1
+                
+        return i == j
